5953759f3d346f56a2a838b23a14114a8893880b
Sean Carlyle <scaryle@ucsc.edu>
2022-07-13 02:31:40 -1000

    working api fetch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const apiurl = 'https://pokeapi.co/api/v2/pokemon/ditto'
+  const [pokemon, setPokemon] = useState(null)
+
+  useEffect(() => {
+    axios.get(apiurl)
+      .then(response => {
+        setPokemon(response.data)
+      })
+  }, [apiurl])
+  
+  if(pokemon){
+    return (
+      <div className="App">
+        {pokemon.height}
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
