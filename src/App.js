5bf10bd400040b5b2a7792eb8e527756616d8966
Sean Carlyle <scaryle@ucsc.edu>
2022-07-14 16:30:05 -1000

    working text changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,14 @@ import axios from 'axios';
 function App() {
   const apiurl = 'https://pokeapi.co/api/v2/pokemon/ditto'
   const [pokemon, setPokemon] = useState(null)
+  const [usertext, setUsertext] = useState('');
 
+  const getInputValue = (e)=>{
+    const userValue = e.target.value;
+    console.log({userValue})
+    setUsertext(userValue)
+  };
+  
   useEffect(() => {
     axios.get(apiurl)
       .then(response => {
@@ -16,7 +23,11 @@ function App() {
   if(pokemon){
     return (
       <div className="App">
-        {pokemon.name}
+        <label>
+          Search: <input type="text" onChange={getInputValue} value={usertext}/>
+        </label>
+        <p>{pokemon.name}</p>
+        <p>{usertext}</p>
       </div>
     );
   }
@@ -28,3 +39,4 @@ function App() {
 }
 
 export default App;
+
